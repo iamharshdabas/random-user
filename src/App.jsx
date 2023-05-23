315d326bd81f334073e6afa96a2d4c7e2a4c0021
Harsh Dabas <iamharshdabas@gmail.com>
2023-05-23 19:19:28 +0530

    ADD: more user info

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,42 @@ const App = () => {
       <button type='button' onClick={fetchRandomUser}>
         Fetch
       </button>
-      {user.name && <h1>{user.name.first}</h1>}
+      {user.name && (
+        <div>
+          <ul>
+            <li>Gender: {user.gender}</li>
+            <li>
+              Name: {user.name.title} {user.name.first} {user.name.last}
+            </li>
+            <li>
+              Location:
+              <ul>
+                <li>
+                  Address: {user.location.street.number}{' '}
+                  {user.location.street.name}, {user.location.city},{' '}
+                  {user.location.state}, {user.location.country}{' '}
+                  {user.location.postcode}
+                </li>
+                <li>
+                  Coordinates: {user.location.coordinates.latitude}{' '}
+                  {user.location.coordinates.longitude}
+                </li>
+                <li>
+                  Timezone: {user.location.timezone.offset}{' '}
+                  {user.location.timezone.description}
+                </li>
+              </ul>
+            </li>
+            <li>Email: {user.email}</li>
+            <li> DOB: {user.dob.date} </li>
+            <li>Age: {user.dob.age} </li>
+            <li>Phone: {user.phone}</li>
+            <li>
+              Picture: <img src={user.picture.large} alt='large' />
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
