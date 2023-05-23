179483644fba740d16652c58e9761cf5462c1bdf
Harsh Dabas <iamharshdabas@gmail.com>
2023-05-23 18:03:27 +0530

    ADD: async to axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import axios from 'axios'
 
 const App = () => {
   const [user, setUser] = useState({})
-  const getRandomUser = () => {
-    axios
-      .get('https://randomuser.me/api/')
-      .then((response) => setUser(response.data.results[0]))
+  const fetchRandomUser = async () => {
+    const response = await axios.get('https://randomuser.me/api/')
+    setUser(response.data.results[0])
   }
+
   return (
     <div>
-      <button type='button' onClick={getRandomUser}>
+      <button type='button' onClick={fetchRandomUser}>
         Fetch
       </button>
       {user.name && <h1>{user.name.first}</h1>}
